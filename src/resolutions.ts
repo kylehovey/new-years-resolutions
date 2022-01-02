fded8bf833c6528d2ca2eb186ad2f5be96bc9ad3
kylehovey <khoveyc@gmail.com>
2022-01-01 23:49:10 -0800

    Added more resolutions

--- src/resolutions.ts
+++ src/resolutions.ts
@@ -10,6 +10,14 @@ const resolutions: Resolution[] = [
      },
      type: ResolutionType.progress,
    },{
+     goal : "Gain 10 pounds",
+     progressBar: {
+       target : 10,
+       units : "lbs",
+       progress : 0,
+     },
+     type: ResolutionType.progress,
+   },{
      goal : "Get VO2 max above 50",
      type: ResolutionType.completion,
      done: false,
@@ -22,13 +30,9 @@ const resolutions: Resolution[] = [
      type: ResolutionType.completion,
      done: false,
    },{
-     goal : "Gain 10 pounds",
-     progressBar: {
-       target : 10,
-       units : "lbs",
-       progress : 0,
-     },
-     type: ResolutionType.progress,
+     goal : "Be flexible enough to do a full forward fold (head to knees)",
+     done: false,
+     type: ResolutionType.completion,
    },{
      goal : "Learn 3 programming languages",
      progressBar: {
@@ -38,7 +42,19 @@ const resolutions: Resolution[] = [
      },
      type: ResolutionType.progress,
    },{
+     goal : "Read 3 fiction books",
+     progressBar: {
+       target : 3,
+       units : "books",
+       progress : 0,
+     },
+     type: ResolutionType.progress,
+   },{
      goal : "Deploy one full-stack application for public use",
+     done: false,
+     type: ResolutionType.completion,
+   },{
+     goal : "Design and order a circuit board",
      done: false,
      type: ResolutionType.completion,
    }
